e6085dbaeb992cce958bbab824b4335e9fb3254b
Riley <increaseblue666@gmail.com>
2022-02-24 17:30:12 +0900

    Feat: 로그인 페이지 틀 작성

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}>
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={password} onChange={onPasswordHandler} />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
