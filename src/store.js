c3c83ad3a1418dff5356b9ed7edb5b01db7a767b
Riley <increaseblue666@gmail.com>
2022-02-26 23:19:27 +0900

    Modify: 사용하지 않는 패키지 삭제

--- src/store.js
+++ src/store.js
@@ -1,13 +1,9 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import promiseMiddleWare from 'redux-promise';
 import rootReducer from './_reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk, promiseMiddleWare))
-);
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
