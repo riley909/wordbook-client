2aed802c3f1feff940352f9396d3addad8b484b7
Riley <increaseblue666@gmail.com>
2022-02-26 23:16:53 +0900

    Modify: 로딩 상태 디스패치 추가

--- src/_actions/user_action.js
+++ src/_actions/user_action.js
@@ -1,13 +1,22 @@
 import { LOGIN } from './types';
 import * as api from '../utils/api';
 
-export const login = (data) => async (dispatch) => {
-  dispatch({ type: LOGIN });
-  try {
-    const res = await api.login(data);
-    dispatch({ type: `${LOGIN}_SUCCESS`, payload: res.data });
-    return res.data;
-  } catch (error) {
-    dispatch({ type: `${LOGIN}_FAILURE`, payload: error });
-  }
+export const login = (data) => {
+  let isLoading = true;
+
+  return async (dispatch) => {
+    dispatch({ type: LOGIN });
+    dispatch({ type: `${LOGIN}_LOADING`, payload: isLoading });
+    try {
+      const res = await api.login(data);
+      dispatch({ type: `${LOGIN}_SUCCESS`, payload: res.data });
+      isLoading = false;
+      dispatch({ type: `${LOGIN}_LOADING`, payload: isLoading });
+      return res.data;
+    } catch (error) {
+      dispatch({ type: `${LOGIN}_FAILURE`, payload: error });
+      isLoading = false;
+      dispatch({ type: `${LOGIN}_LOADING`, payload: isLoading });
+    }
+  };
 };
