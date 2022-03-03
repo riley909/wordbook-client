9ef325ad83825abb1b3c89210853cd34cf8eee06
Riley <increaseblue666@gmail.com>
2022-03-03 16:57:51 +0900

    Feat: 이메일로 유저 조회 api 작성

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 const baseUrl = `http://localhost:4000`;
 
 export const login = async (data) => await axios.post(`${baseUrl}/auth/signin`, data);
-
 export const signup = async (data) => await axios.post(`${baseUrl}/auth/signup`, data);
+export const getEmail = async (email) =>
+  await axios.get(`${baseUrl}/auth/email/${email}`);
