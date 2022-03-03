97572abb37a1d36ce982ab6dd6d12d461d4aeb4a
Riley <increaseblue666@gmail.com>
2022-03-03 16:58:24 +0900

    Feat: 이메일 중복 조회 메서드 작성

--- src/components/SignupPage/SignupContainer.js
+++ src/components/SignupPage/SignupContainer.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import Signup from './Signup';
 import { signup as signupStart } from '../../_actions/user_action';
+import * as api from '../../utils/api';
 
 export default function SignupContainer() {
   const dispatch = useDispatch();
@@ -18,5 +19,15 @@ export default function SignupContainer() {
     [dispatch, navigate]
   );
 
-  return <Signup signup={signup} />;
+  const emailCheck = async (email) => {
+    try {
+      const response = await api.getEmail(email);
+      if (response.data) return true;
+    } catch (error) {
+      // 조회된 유저 없음
+      return false;
+    }
+  };
+
+  return <Signup signup={signup} emailCheck={emailCheck} />;
 }
