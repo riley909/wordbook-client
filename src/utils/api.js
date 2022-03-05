452375a3dba544949ac1b519c66a9e8a857e5671
Riley <increaseblue666@gmail.com>
2022-03-05 16:14:58 +0900

    Modify: 로그인 요청시 옵션 변경
    - 인증 정보 가져올수 있도록 withCredentials 옵션 허용

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const baseUrl = `http://localhost:4000`;
 
-export const login = async (data) => await axios.post(`${baseUrl}/auth/signin`, data);
+export const login = async (data) =>
+  await axios.post(`${baseUrl}/auth/signin`, data, { withCredentials: true });
 export const signup = async (data) => await axios.post(`${baseUrl}/auth/signup`, data);
 export const getEmail = async (email) =>
   await axios.get(`${baseUrl}/auth/email/${email}`);
