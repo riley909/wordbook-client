44a134eec9466be9e0686f56271c3c1e935d4fb3
Riley <increaseblue666@gmail.com>
2022-03-05 16:16:16 +0900

    Modify: 초기 상태값 변경
    - 쿠키에 토큰이 있으면 initialState에 토큰 할당

--- src/_reducers/initialState.js
+++ src/_reducers/initialState.js
@@ -1,8 +1,16 @@
+import Cookies from 'universal-cookie';
+
+const cookie = new Cookies();
+let token = null;
+if (cookie.get('Authorization')) {
+  token = cookie.get('Authorization');
+}
+
 export const initialState = {
   auth: {
     loading: false,
     error: null,
-    token: null,
+    token: token,
   },
   signup: {
     loading: false,
