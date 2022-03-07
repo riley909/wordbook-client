dcefe6ba5309ac89941c184be8be7cf5076ff1f2
Riley <increaseblue666@gmail.com>
2022-03-07 22:04:36 +0900

    Modify: 컴포넌트 이름 변경

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,7 +1,7 @@
 import { Button, PageHeader } from 'antd';
 import Search from 'antd/lib/input/Search';
 import { useSelector } from 'react-redux';
-import Layout from './Layout';
+import LayoutTop from './LayoutTop';
 import styles from '../../styles/Home.module.css';
 import { FaBookMedical } from 'react-icons/fa';
 import { getQuery } from '../../utils/api';
@@ -58,7 +58,7 @@ export default function Home({ search, login, signup, logout, mybook }) {
           ]}
         />
       )}
-      <Layout>
+      <LayoutTop>
         <div className={styles.search_area}>
           <div className={styles.search_title}>[한국어 - 인도네시아어 사전]</div>
           <div className={styles.search_subtitle}>
@@ -72,7 +72,8 @@ export default function Home({ search, login, signup, logout, mybook }) {
             ref={searchRef}
           />
         </div>
-      </Layout>
+        <div></div>
+      </LayoutTop>
     </div>
   );
 }
---
+++ src/components/HomePage/LayoutTop.js
@@ -0,0 +1,14 @@
+import { Col, Row } from 'antd';
+import styles from '../../styles/LayoutTop.module.css';
+
+const LayoutTop = ({ children }) => (
+  <Row align="middle" className={styles.layout_top_row}>
+    <Col span={24}>
+      <Row justify="center" className={styles.layout_top_contents}>
+        <Col span={12}>{children}</Col>
+      </Row>
+    </Col>
+  </Row>
+);
+
+export default LayoutTop;
