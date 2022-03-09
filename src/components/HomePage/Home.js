32ef9a41573c0b36104c240bbf5a579547604ec1
Riley <increaseblue666@gmail.com>
2022-03-09 23:54:13 +0900

    Feat: 슬라이드 이동 효과 적용

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -6,7 +6,7 @@ import LayoutBottom from './LayoutBottom';
 import styles from '../../styles/Home.module.css';
 import { FaBookMedical } from 'react-icons/fa';
 import { getQuery } from '../../utils/api';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import TodaysWord from './TodaysWord';
 import MyQuiz from './MyQuiz';
 import Encyclopedia from './Encyclopedia';
@@ -15,6 +15,11 @@ import NextButton from './NextButton';
 export default function Home({ search, login, signup, logout, mybook }) {
   const token = useSelector((state) => state.user.auth.token);
   const searchRef = useRef(null);
+
+  // 슬라이드 개수(0부터 시작)
+  const totalSlides = 1;
+  const slideRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const onSearch = () => {
     const q = searchRef.current.state.value;
@@ -26,8 +31,34 @@ export default function Home({ search, login, signup, logout, mybook }) {
     }
   };
 
-  const prev = () => {};
-  const next = () => {};
+  const prev = () => {
+    // 현재 1번 슬라이드인데 이전 버튼 누를경우
+    if (currentSlide === 0) {
+      // 마지막 슬라이드로 이동
+      setCurrentSlide(totalSlides);
+    } else {
+      // 아니면 이전 슬라이드로 이동
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  const next = () => {
+    // 더 넘어갈 슬라이드가 없으면
+    if (currentSlide >= totalSlides) {
+      // 1번 슬라이드로 이동
+      setCurrentSlide(0);
+    } else {
+      // 아니면 다음 슬라이드로 이동
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  useEffect(() => {
+    // css overflow = hidden으로 영역 밖의 슬라이드 숨김
+    // 슬라이드 이동하는 애니메이션 적용
+    slideRef.current.style.transition = 'all 0.5s ease-in-out';
+    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`;
+  }, [currentSlide]);
 
   return (
     <div>
@@ -89,26 +120,28 @@ export default function Home({ search, login, signup, logout, mybook }) {
           <Divider className={styles.divider} />
           <div>
             <div className={styles.pedia_area}>
-              <Encyclopedia
-                url1="https://terms.naver.com/entry.naver?docId=3595492&cid=58250&categoryId=58250"
-                title1="인도네시아어의 갈래와 사용현황"
-                url2="https://terms.naver.com/entry.naver?docId=3595494&categoryId=58250&cid=58250"
-                title2="인도네시아어의 기본 문법"
-                url3="https://terms.naver.com/entry.naver?docId=3595493&categoryId=58250&cid=58250"
-                title3="인도네시아어의 문자와 발음"
-                url4="https://terms.naver.com/entry.naver?docId=3595495&categoryId=58250&cid=58250"
-                title4="인도네시아어의 인사말과 기본표현"
-              />
-              <Encyclopedia
-                url1="https://terms.naver.com/entry.naver?docId=3595496&categoryId=58250&cid=58250"
-                title1="인도네시아어의 언어적 특징"
-                url2="https://terms.naver.com/entry.naver?docId=3595497&categoryId=58250&cid=58250"
-                title2="인도네시아어의 발달과 역사"
-                url3="https://terms.naver.com/entry.naver?docId=3595520&categoryId=58250&cid=58250"
-                title3="인도네시아어의 숫자와 날짜"
-                url4={null}
-                title4={null}
-              />
+              <div ref={slideRef} className={styles.pedia_container}>
+                <Encyclopedia
+                  url1="https://terms.naver.com/entry.naver?docId=3595492&cid=58250&categoryId=58250"
+                  title1="인도네시아어의 갈래와 사용현황"
+                  url2="https://terms.naver.com/entry.naver?docId=3595494&categoryId=58250&cid=58250"
+                  title2="인도네시아어의 기본 문법"
+                  url3="https://terms.naver.com/entry.naver?docId=3595493&categoryId=58250&cid=58250"
+                  title3="인도네시아어의 문자와 발음"
+                  url4="https://terms.naver.com/entry.naver?docId=3595495&categoryId=58250&cid=58250"
+                  title4="인도네시아어의 인사말과 기본표현"
+                />
+                <Encyclopedia
+                  url1="https://terms.naver.com/entry.naver?docId=3595496&categoryId=58250&cid=58250"
+                  title1="인도네시아어의 언어적 특징"
+                  url2="https://terms.naver.com/entry.naver?docId=3595497&categoryId=58250&cid=58250"
+                  title2="인도네시아어의 발달과 역사"
+                  url3="https://terms.naver.com/entry.naver?docId=3595520&categoryId=58250&cid=58250"
+                  title3="인도네시아어의 숫자와 날짜"
+                  url4={null}
+                  title4={null}
+                />
+              </div>
             </div>
             <NextButton prev={prev} next={next} />
           </div>
