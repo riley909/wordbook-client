feb723666da03ec49b9f095df759d40406b4e457
Riley <increaseblue666@gmail.com>
2022-03-10 13:13:52 +0900

    Modify: css 클래스네임 변경

--- src/components/HomePage/MyTest.js
+++ src/components/HomePage/MyTest.js
@@ -1,21 +1,28 @@
 import styles from '../../styles/MyTest.module.css';
 import { FaAngleRight } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
+import { Progress } from 'antd';
 
-const MyTest = () => {
+const MyTest = ({ login }) => {
   const token = useSelector((state) => state.user.auth.token);
 
   return (
     <div>
-      <div className={styles.quiz_area}>
-        <div className={styles.quiz_title}>
+      <div className={styles.test_area}>
+        <div className={styles.test_title}>
           <div>나의 테스트</div>
-          <FaAngleRight className={styles.quiz_title_icon} />
+          <FaAngleRight className={styles.test_title_icon} />
         </div>
         {!token ? (
-          <div className={styles.quiz_contents}>내 성적 확인하러 가기!</div>
+          <div className={styles.test_contents}>
+            <div>
+              <div>2021-03-09</div>
+              <Progress type="circle" percent={50} format={(percent) => `5 / 10`} />
+            </div>
+            <div onClick={login}>내 성적 확인하기 </div>
+          </div>
         ) : (
-          <div className={styles.quiz_contents}>내용</div>
+          <div className={styles.test_contents}>내 성적</div>
         )}
       </div>
     </div>
