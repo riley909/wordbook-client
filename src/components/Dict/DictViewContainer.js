589277e23f5fc0d66f4e5812022e7575957ff6d2
Riley <increaseblue666@gmail.com>
2022-03-15 22:51:43 +0900

    Feat: fetchData 함수 작성
    - url변경에 따라 dispatch하도록 useEffect 훅 사용

--- src/components/Dict/DictViewContainer.js
+++ src/components/Dict/DictViewContainer.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DictView from './DictView';
+import QueryString from 'qs';
+import { useLocation, useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { searchView as searchViewStart } from '../../_actions/dict_action';
 
 export default function DictViewContainer() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const queryData = QueryString.parse(location.search, { ignoreQueryPrefix: true });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch(await searchViewStart(queryData.target_code));
+    };
+    fetchData();
+
+    // queryData가 바뀔때마다 dispatch
+  }, [dispatch, queryData]);
+
   return <DictView />;
 }
