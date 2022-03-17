db1d8b01c0a2953a3e6c3229a62ed7f2c9fe7e71
Riley <increaseblue666@gmail.com>
2022-03-17 12:36:46 +0900

    Feat: 로딩 컴포넌트 분리

---
+++ src/components/Dict/Loading.js
@@ -0,0 +1,18 @@
+import { Spin } from 'antd';
+import React from 'react';
+import Header from '../NavBar/Header';
+import { LoadingOutlined } from '@ant-design/icons';
+import styles from '../../styles/Loading.module.css';
+
+export default function Loading() {
+  const loadingIcon = <LoadingOutlined spin />;
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.loading_area}>
+        <Spin tip="Loading..." indicator={loadingIcon} style={{ fontSize: 18 }} />
+      </div>
+    </div>
+  );
+}
