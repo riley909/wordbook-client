0f033cce10974e44021d8ea39b291dd0541c99cf
Riley <increaseblue666@gmail.com>
2022-03-17 14:51:21 +0900

    Feat: 예문 더보기 기능

--- src/components/Dict/ExampleInfo.js
+++ src/components/Dict/ExampleInfo.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
+import styles from '../../styles/DictView.module.css';
 
 export default function ExampleInfo({ example_info }) {
+  const limit = 5;
   let arrSort = example_info.sort((a, b) => {
     return a.example.length - b.example.length;
   });
+  const arrLimit = arrSort.slice(0, limit + 1);
+  const [isOpen, setIsOpen] = useState(false);
+  const [moreData, setMoreData] = useState(arrLimit);
 
-  // !example_info 컴포넌트(노출 5개, 더보기 기능)
+  const onShowMoreClick = () => {
+    setIsOpen((prev) => !prev);
+    setMoreData((prev) => {
+      if (prev === arrLimit) return arrSort;
+      else return arrLimit;
+    });
+  };
 
   return (
     <div>
-      <ul>
-        {arrSort.map((el) => (
-          <li>{el.example}</li>
-        ))}
-      </ul>
-      <div>
-        더 보기 <FaCaretDown />
-      </div>
-      <div>
-        닫기 <FaCaretUp />
-      </div>
+      {arrSort.length <= limit ? (
+        <ul>
+          {arrSort.map((el) => (
+            <li>{el.example}</li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {moreData.map((el) => (
+            <li>{el.example}</li>
+          ))}
+        </ul>
+      )}
+
+      {arrSort.length > limit && (
+        <div className={styles.example_more_area} onClick={onShowMoreClick}>
+          <div className={!isOpen ? null : styles.example_hide}>
+            더 보기 <FaCaretDown />
+          </div>
+          <div className={isOpen ? null : styles.example_hide}>
+            닫기 <FaCaretUp />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
