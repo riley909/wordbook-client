b857e9a1e93af788428255050d44e360eefb7251
Riley <increaseblue666@gmail.com>
2022-03-18 18:02:31 +0900

    Modify: 최근 검색어 컴포넌트 이름 변경

--- src/components/NavBar/Side/History.js
+++ src/components/NavBar/Side/History.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from '../../../styles/NavBar/Side.module.css';
 
-export default function SearchWords() {
+export default function History() {
   return (
     <div>
-      <div className={styles.search_words_title}>내가 찾은 단어</div>
+      <div className={styles.history_title}>내가 찾은 단어</div>
       <div>최근 단어 5개</div>
       <button className={styles.button}>내 단어장 가기</button>
     </div>
