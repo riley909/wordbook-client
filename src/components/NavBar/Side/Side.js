7c9d37f6b8dcf3ae5ad52679cb95515a90bae0f3
Riley <increaseblue666@gmail.com>
2022-03-19 23:14:40 +0900

    Feat: 단어장 페이지 팝업
    - 토큰 여부에 따라 로그인화면 이동 또는 단어장 팝업

--- src/components/NavBar/Side/Side.js
+++ src/components/NavBar/Side/Side.js
@@ -3,20 +3,51 @@ import React from 'react';
 import styles from '../../../styles/NavBar/Side.module.css';
 import LatestPosts from './LatestPosts';
 import History from './History';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 
 export default function Side() {
+  const token = useSelector((state) => state.user.auth.token);
+  const navigate = useNavigate();
+
+  const toLogin = () => navigate('/login');
+  const toWordBook = () => {
+    window.open('/wordBook', '단어장', 'width=450, height=700, scrollbars=yes');
+  };
+
   return (
-    <div>
-      <div>
-        <span>단어장</span>
-        <Divider type="vertical" className={styles.divider_vertical} />
-        <span>스터디로그</span>
-      </div>
-      <Divider className={styles.divider} />
-      <LatestPosts />
-      <Divider className={styles.divider} />
-      <History />
-      <Divider className={styles.divider} />
-    </div>
+    <>
+      {token ? (
+        <div>
+          <div>
+            <span onClick={toWordBook} className={styles.side_wordbook}>
+              단어장
+            </span>
+            <Divider type="vertical" className={styles.divider_vertical} />
+            <span>스터디로그</span>
+          </div>
+          <Divider className={styles.divider} />
+          <LatestPosts />
+          <Divider className={styles.divider} />
+          <History />
+          <Divider className={styles.divider} />
+        </div>
+      ) : (
+        <div>
+          <div>
+            <span onClick={toLogin} className={styles.side_wordbook}>
+              단어장
+            </span>
+            <Divider type="vertical" className={styles.divider_vertical} />
+            <span>스터디로그</span>
+          </div>
+          <Divider className={styles.divider} />
+          <LatestPosts />
+          <Divider className={styles.divider} />
+          <History />
+          <Divider className={styles.divider} />
+        </div>
+      )}
+    </>
   );
 }
