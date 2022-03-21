ecbb34e53586476ad912a4b1098dda54d068c836
Riley <increaseblue666@gmail.com>
2022-03-21 19:02:51 +0900

    Feat: 아이콘 클릭시 모달 팝업

--- src/components/WordBook/WordBook.js
+++ src/components/WordBook/WordBook.js
@@ -1,5 +1,5 @@
-import { Divider, PageHeader } from 'antd';
-import React from 'react';
+import { Button, Divider, Modal, PageHeader } from 'antd';
+import React, { useState } from 'react';
 import { FaBookMedical } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
@@ -7,14 +7,32 @@ import styles from '../../styles/WordBook.module.css';
 import { FaFolderPlus } from 'react-icons/fa';
 import { BiCog } from 'react-icons/bi';
 
-export default function WordBook({ home, wordbook }) {
+export default function WordBook({ home, wordbook, handleOk }) {
   const navigate = useNavigate();
   const token = useSelector((state) => state.user.auth.token);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
 
   // 토큰이 없고, 팝업으로 접속하지 않을 경우
   if (!token) {
     navigate('/');
   }
+
+  const openModal = () => {
+    setVisible(true);
+  };
+
+  const onOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const onCancel = () => {
+    setVisible(false);
+  };
 
   return (
     <div className={styles.container}>
@@ -38,7 +56,7 @@ export default function WordBook({ home, wordbook }) {
         <div className={styles.list_title_area}>
           <div className={styles.list_title}>단어장 목록</div>
           <div className={styles.list_icon_area}>
-            <FaFolderPlus className={styles.list_add_icon} />
+            <FaFolderPlus className={styles.list_add_icon} onClick={openModal} />
             <BiCog className={styles.list_settings_icon} />
           </div>
         </div>
@@ -52,6 +70,22 @@ export default function WordBook({ home, wordbook }) {
             <span>폴더</span> <span className={styles.list_item_counter}>0</span>
           </div>
         </div>
+
+        <Modal
+          title="새 폴더 추가"
+          visible={visible}
+          confirmLoading={confirmLoading}
+          onCancel={onCancel}
+          footer={[
+            <Button key="back" onClick={onCancel}>
+              취소
+            </Button>,
+            <Button key="submit" type="primary" onClick={onOk}>
+              확인
+            </Button>,
+          ]}>
+          <div>추가</div>
+        </Modal>
       </div>
     </div>
   );
