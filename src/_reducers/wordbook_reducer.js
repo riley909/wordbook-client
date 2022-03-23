6b623e2d848770bdaf1594ab7f007f8be565f7c4
Riley <increaseblue666@gmail.com>
2022-03-23 22:16:00 +0900

    Feat: getFolderList 리듀서 작성

--- src/_reducers/wordbook_reducer.js
+++ src/_reducers/wordbook_reducer.js
@@ -1,8 +1,9 @@
-import { CREATE_FOLDER } from '../_actions/types';
+import { CREATE_FOLDER, GET_FOLDER_LIST } from '../_actions/types';
 
 export const wordbookState = {
   folder: {
     create: { error: null, data: null },
+    read: { loading: false, error: null, data: null },
   },
 };
 export default function (state = wordbookState, action) {
@@ -16,6 +17,38 @@ export default function (state = wordbookState, action) {
       return {
         ...state,
         folder: { create: { error: action.payload, data: null } },
+      };
+    case `${GET_FOLDER_LIST}_LOADING`:
+      return {
+        ...state,
+        folder: {
+          read: {
+            ...state.folder.read,
+            loading: action.payload,
+          },
+        },
+      };
+    case `${GET_FOLDER_LIST}_SUCCESS`:
+      return {
+        ...state,
+        folder: {
+          read: {
+            loading: false,
+            error: null,
+            data: action.payload,
+          },
+        },
+      };
+    case `${GET_FOLDER_LIST}_FAILURE`:
+      return {
+        ...state,
+        folder: {
+          read: {
+            loading: false,
+            error: action.payload,
+            data: null,
+          },
+        },
       };
 
     default:
