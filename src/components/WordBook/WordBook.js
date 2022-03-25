0a118793640eab27023555309e03ce68e1d0cc0d
Riley <increaseblue666@gmail.com>
2022-03-25 18:10:29 +0900

    Modify: useEffect 의존성배열 추가

--- src/components/WordBook/WordBook.js
+++ src/components/WordBook/WordBook.js
@@ -24,7 +24,7 @@ export default function WordBook({ home, wordbook, handleOk, getFolderList }) {
 
   useEffect(() => {
     getFolderList();
-  }, []);
+  }, [getFolderList]);
 
   console.log(folderList);
 
