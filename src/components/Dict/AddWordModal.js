4b24da71d8c7301edd60ceefbe41daf8d2e0343e
Riley <increaseblue666@gmail.com>
2022-03-29 00:19:07 +0900

    Modify: 라디오버튼 기능 수정
    - 라디오버튼 클릭시 value와 일치하는것만 check상태로 함
    - 단어 추가 함수 바디 수정

--- src/components/Dict/AddWordModal.js
+++ src/components/Dict/AddWordModal.js
@@ -1,15 +1,19 @@
 import { Divider, Modal, Radio } from 'antd';
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFolderList as getFolderListStart } from '../../_actions/wordbook_action';
 import LoadingWithOutHeader from '../Loading/LoadingWithOutHeader';
 import styles from '../../styles/AddWordModal.module.css';
 import { FaAngleRight } from 'react-icons/fa';
 
-export default function AddWordModal({ visible, setVisible, addClick }) {
+export default function AddWordModal({ visible, setVisible, target_code, addClick }) {
   const loading = useSelector((state) => state.wordbook.folder.loading);
-  const folderList = useSelector((state) => state.wordbook.folder.data);
+  const folderList = useSelector(
+    (state) => state.wordbook.folder.data && state.wordbook.folder.data
+  );
   const dispatch = useDispatch();
+  const [value, setValue] = useState(folderList && folderList[0].id);
+
   const getFolderList = useCallback(async () => {
     dispatch(await getFolderListStart());
   }, [dispatch]);
@@ -18,8 +22,17 @@ export default function AddWordModal({ visible, setVisible, addClick }) {
     getFolderList();
   }, [getFolderList]);
 
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
   const onAddClick = () => {
-    addClick();
+    const body = {
+      target_code,
+      folderId: value,
+    };
+    addClick(body);
+    setVisible(false);
   };
 
   const onCancel = () => {
@@ -44,12 +57,16 @@ export default function AddWordModal({ visible, setVisible, addClick }) {
               <div className={styles.list_title}>단어장 목록</div>
               <Divider className={styles.list_divider} />
               <div className={styles.list_item_area}>
-                {folderList.map((val, idx) => {
+                {folderList.map((val) => {
                   const count = val.words ? val.words.length : 0;
                   return (
                     <div key={val.id}>
                       <div className={styles.list_item} id={val.id}>
-                        {idx === 0 ? <Radio defaultChecked /> : <Radio />}
+                        <Radio
+                          onChange={onChange}
+                          value={val.id}
+                          checked={val.id === value}
+                        />
                         <span>{val.name}</span>
                         <span className={styles.list_item_counter}>{count}</span>
                         <div
