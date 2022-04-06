e78b0348d9b98664b04c6b74439b2a111085369f
Riley <increaseblue666@gmail.com>
2022-04-06 20:31:26 +0900

    Feat:  getStudyLogs 함수 적용

---
+++ src/components/StudyLog/StudyLogListContainer.js
@@ -0,0 +1,21 @@
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import StudyLogList from './StudyLogList';
+import { getStudyLogs as getStudyLogsStart } from '../../_actions/studylog_action';
+
+export default function StudyLogListContainer() {
+  const dispatch = useDispatch();
+
+  const getStudyLogs = useCallback(
+    async (search, date, limit, offset) => {
+      await dispatch(getStudyLogsStart(search, date, limit, offset));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    getStudyLogs('', '', 10, 1);
+  });
+
+  return <StudyLogList getStudyLogs={getStudyLogs} />;
+}
