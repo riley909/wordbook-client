1d45b71e88d5af3e5d3898fdcdb48901b5d03975
Riley <increaseblue666@gmail.com>
2022-04-06 20:27:27 +0900

    Feat: 최근 작성글 없는 사이드바 생성

---
+++ src/components/NavBar/Side/SideWithOutPosts.js
@@ -0,0 +1,51 @@
+import { Divider } from 'antd';
+import React from 'react';
+import styles from '../../../styles/NavBar/Side.module.css';
+import History from './History';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+
+export default function Side({ email }) {
+  const token = useSelector((state) => state.user.auth.token);
+  const navigate = useNavigate();
+
+  const toLogin = () => navigate('/login');
+  const toWordBook = () => {
+    window.open('/wordbook', '단어장', 'width=450, height=700, scrollbars=yes');
+  };
+
+  return (
+    <>
+      {token ? (
+        <div>
+          <div>
+            <div className={styles.side_email_area}>
+              <span className={styles.side_email}>{email}</span> 님의
+            </div>
+            <span onClick={toWordBook} className={styles.side_wordbook}>
+              단어장
+            </span>
+            <Divider type="vertical" className={styles.divider_vertical} />
+            <span>스터디로그</span>
+          </div>
+          <Divider className={styles.divider} />
+          <History />
+          <Divider className={styles.divider} />
+        </div>
+      ) : (
+        <div>
+          <div>
+            <span onClick={toLogin} className={styles.side_wordbook}>
+              단어장
+            </span>
+            <Divider type="vertical" className={styles.divider_vertical} />
+            <span>스터디로그</span>
+          </div>
+          <Divider className={styles.divider} />
+          <History />
+          <Divider className={styles.divider} />
+        </div>
+      )}
+    </>
+  );
+}
