38982575e4bd44136bafd429e9d9a26a8e069a8f
Riley <increaseblue666@gmail.com>
2022-04-10 22:03:46 +0900

    Feat: deleteStudyLog 리듀서 작성

--- src/_reducers/studylog_reducer.js
+++ src/_reducers/studylog_reducer.js
@@ -1,4 +1,4 @@
-import { CREATE_STUDYLOG, GET_STUDYLOGS } from '../_actions/types';
+import { CREATE_STUDYLOG, DELETE_STUDYLOG, GET_STUDYLOGS } from '../_actions/types';
 
 export const studyLogState = {
   loading: false,
@@ -42,6 +42,28 @@ export default function (state = studyLogState, action) {
         },
       };
     case `${CREATE_STUDYLOG}_FAILURE`:
+      return {
+        loading: false,
+        error: action.payload,
+        data: null,
+      };
+    case `${DELETE_STUDYLOG}_LOADING`:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case `${DELETE_STUDYLOG}_SUCCESS`:
+      return {
+        loading: false,
+        error: null,
+        data: {
+          ...state.data,
+          total: state.data.total - 1,
+          currentPage: 1,
+          data: state.data.data.filter((val) => val.id !== action.payload),
+        },
+      };
+    case `${DELETE_STUDYLOG}_FAILURE`:
       return {
         loading: false,
         error: action.payload,
