c5a55e2d158175fb8aedfaf70faf905701e3d072
Riley <increaseblue666@gmail.com>
2022-04-20 00:33:19 +0900

    Feat: 최근 작성글 가져오기

--- src/components/NavBar/Side/LatestPosts.js
+++ src/components/NavBar/Side/LatestPosts.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import styles from '../../../styles/NavBar/Side.module.css';
+import { getStudyLogs as getStudyLogsStart } from '../../../_actions/studylog_action';
 
 export default function LatestPosts({ token }) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const studyLogList = useSelector(
+    (state) => state.studylog.data && state.studylog.data.data
+  );
+  const [latestStudyLogs, setLatestStudyLogs] = useState(null);
+
   const toLogin = () => navigate('/login');
   const toStudyLog = () => navigate('/studyLog');
+
+  const getStudyLogs = useCallback(
+    async (search, date, limit, offset) => {
+      await dispatch(getStudyLogsStart(search, date, limit, offset));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    getStudyLogs('', '', 5, 1);
+  }, []);
+
+  useEffect(() => {
+    setLatestStudyLogs(studyLogList);
+  }, [studyLogList]);
 
   return (
     <div>
       <div className={styles.latest_posts_title}>최근 작성한 글</div>
-      <div>글 5개</div>
+      <div>
+        {latestStudyLogs && (
+          <>
+            {latestStudyLogs.map((val) => {
+              const createdAt = val.createdAt.split('.')[0].split('T');
+              return (
+                <div key={val.id} className={styles.latest_posts_item}>
+                  <div className={styles.latest_posts_date}>{createdAt}</div>
+                  <div className={styles.latest_posts_content}>{val.content}</div>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
       {token ? (
         <button onClick={toStudyLog} className={styles.button}>
           새 스터디로그 쓰기
