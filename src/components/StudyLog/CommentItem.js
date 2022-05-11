42de9cccf252655cafbe079e767fef7dfaeb852b
Riley <increaseblue666@gmail.com>
2022-05-11 19:49:58 +0900

    Fix: 댓글 불러오기 오류 수정
    - 이전에 열었던 글의 댓글이 보이는 오류 수정

--- src/components/StudyLog/CommentItem.js
+++ src/components/StudyLog/CommentItem.js
@@ -1,32 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../../styles/Comment.module.css';
 import { FaTimes } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import LoadingWithOutHeader from '../Loading/LoadingWithOutHeader';
 
-export default function CommentItem({ getComments, studyLogId, comments, setComments }) {
-  const commentList = useSelector((state) => state.comment.data);
-  const total = commentList && commentList.total;
-  const limit = commentList && commentList.limit;
-  const lastPage = Math.ceil(total / limit);
-  const currentPage = commentList && commentList.currentPage;
+export default function CommentItem({
+  getComments,
+  studyLogId,
+  comments,
+  setComments,
+  COMMENT_LIMIT,
+}) {
+  const commentList = useSelector(
+    (state) => state.comment.data && state.comment.data.data
+  );
+  const id = useSelector((state) => commentList && state.comment.data.studyLogId);
+  const page = useSelector((state) => commentList && state.comment.data.currentPage);
+  const currentPage = Number(page);
+  const total = useSelector((state) => commentList && state.comment.data.total);
+  const lastPage = Math.ceil(total / COMMENT_LIMIT);
+  const [logId, setLogId] = useState(id);
 
   useEffect(() => {
-    setComments((prev) => {
-      if (!prev && currentPage === 1) {
-        return commentList;
-      } else {
-        if (commentList) {
-          return Object.assign(prev, { data: prev.data.concat(commentList.data) });
+    setLogId(Number(id));
+  }, [id]);
+
+  useEffect(() => {
+    if (studyLogId === logId) {
+      setComments((prev) => {
+        // 현재 페이지가 1임
+        // = 최초로 댓글창 열었을 때
+        // = or 닫기전에 확인한 페이지가 1임
+        if (currentPage === 1) {
+          return commentList;
         }
-      }
-    });
-  }, [commentList]);
+        // 이전 데이터가 있고 현재 페이지가 1이 아님
+        // = 더보기 버튼을 눌렀을 때
+        if (prev && currentPage !== 1) {
+          return prev.concat(commentList);
+        }
+        // else 이전 데이터도 없고 현재 페이지가 1이 아님
+        // = 댓글창 닫으면 setComments(null) 시키므로 초기화되고
+        //   창 닫기 전에 더보기를 눌러서 현재페이지가 1이 아닌 경우
+        // = 다시 댓글창 열었을 때 마지막에 확인한 페이지가 set되지 않고
+        //   1페이지를 불러올 때 까지 로딩이 뜨게 됨
+      });
+    }
+  }, [commentList, logId]);
 
   const handleMoreComments = (id) => {
     // 페이지가 남아있을때만 데이터 가져옴
     if (currentPage < lastPage) {
-      getComments(id, '', currentPage + 1);
+      getComments(id, COMMENT_LIMIT, currentPage + 1);
     }
   };
 
@@ -35,7 +60,7 @@ export default function CommentItem({ getComments, studyLogId, comments, setComm
       {comments ? (
         <div>
           <div className={styles.item_border}></div>
-          {comments.data.map((val) => {
+          {comments.map((val) => {
             const createdAt = val.createdAt.split('.')[0].split('T');
             return (
               <div key={val.id}>
